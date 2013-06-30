f020c51dd510be338e2716b81a0eb7a471d19c79
Elijah Andrews <elijahcandrews@gmail.com>
2013-06-30 01:03:24 -0400

    Remove FPS limit, add frame-independent movement speed

--- src/main.cpp
+++ src/main.cpp
@@ -22,33 +22,32 @@ int main(int argc, char* args[]) {
 
   // Set up a rectangle for the snake sprite and its inital position
   SDL_Rect snake_segment_rectangle;
-  snake_segment_rectangle.x = 200;
+  snake_segment_rectangle.x = 0;
   snake_segment_rectangle.y = 200;
   snake_segment_rectangle.w = 33;
   snake_segment_rectangle.h = 33;
 
-  Timer loop_timer; // used to limit the framerate
+  Timer delta_timer; // used to calculate velocity
+  delta_timer.Start();
   Timer fps_timer; // used to measure the fps once per second
   int frames = 0; // number of frames that have passed in the last second
   fps_timer.Start();
 
   // Main loop
   while(true) {
-    loop_timer.Start();
 
     // Clear the screen:
     SDL_FillRect(screen, &screen->clip_rect,
                  SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
 
-    snake_segment_rectangle.x += 2; // Move by 2 pixels per frame
+    // Move by 120 pixels / second, regardless of framerate
+    snake_segment_rectangle.x += 440 * (delta_timer.GetTicks() / 1000.f);
     if (snake_segment_rectangle.x >= SCREEN_WIDTH)
       break; // stop when we hit a wall
     SDL_BlitSurface(snake_segment_sprite, NULL, screen,
                     &snake_segment_rectangle);
 
-    // Delay to keep us at 60 fps
-    if(loop_timer.GetTicks() < 1000/FPS)
-      SDL_Delay(1000/FPS - loop_timer.GetTicks());
+    delta_timer.Start();
     SDL_Flip(screen);
 
     // Print the FPS to the command line once per second
